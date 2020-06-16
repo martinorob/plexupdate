104d87fde08d56a638aa2ed16b2f97b348db4b2d
martinorob <m@rtino.info>
2020-06-16 16:46:30 +0200

    Update plexupdate.sh

--- plexupdate.sh
+++ plexupdate.sh
@@ -7,7 +7,8 @@
 # @author @martinorob https://github.com/martinorob
 # https://github.com/martinorob/plexupdate/
 
-mkdir /volume1/plextemp/ > /dev/null 2>&1
+#!/bin/bash
+mkdir -p /tmp/plex/ > /dev/null 2>&1
 token=$(cat /volume1/Plex/Library/Application\ Support/Plex\ Media\ Server/Preferences.xml | grep -oP 'PlexOnlineToken="\K[^"]+')
 url=$(echo "https://plex.tv/api/downloads/5.json?channel=plexpass&X-Plex-Token=$token")
 jq=$(curl -s ${url})
@@ -19,17 +20,13 @@ if [ "$newversion" != "$curversion" ]
 then
 echo New Vers Available
 /usr/syno/bin/synonotify PKGHasUpgrade '{"[%HOSTNAME%]": $(hostname), "[%OSNAME%]": "Synology", "[%PKG_HAS_UPDATE%]": "Plex", "[%COMPANY_NAME%]": "Synology"}'
-cpu=$(uname -m)
-if [ "$cpu" = "x86_64" ]; then
-url=$(echo $jq | jq -r ".nas.Synology.releases[1] | .url")
-else
- url=$(echo $jq | jq -r ".nas.Synology.releases[0] | .url")
-fi
-/bin/wget $url -P /volume1/tmp/plex/
-/usr/syno/bin/synopkg install /volume1/tmp/plex/*.spk
+CPU=$(uname -m)
+url=$(echo "${jq}" | jq -r '.nas.Synology.releases[] | select(.build=="linux-'"${CPU}"'") | .url')
+/bin/wget $url -P /tmp/plex/
+/usr/syno/bin/synopkg install /tmp/plex/*.spk
 sleep 30
 /usr/syno/bin/synopkg start "Plex Media Server"
-rm -rf /volume1/tmp/plex/*
+rm -rf /tmp/plex/*
 else
 echo No New Ver
 fi
